08c065a5454279f0bd693df8d911f88b8b011108
Rommel Rico <me@rommelrico.com>
2019-02-03 15:33:43 -0800

    Showing posts / todos buttons

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import React from 'react';
 class App extends React.Component {
 
     render() {
-        return <div>App</div>;
+        return (
+            <div>
+                <div>
+                    <button>Posts</button>
+                    <button>Todos</button>
+                </div>
+            </div>
+        );
     }
 
 }
