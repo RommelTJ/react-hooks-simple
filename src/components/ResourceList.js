096a4c9eea3a2ef9dcf1cce21fe14b8580ada8ac
Rommel Rico <me@rommelrico.com>
2019-02-04 06:44:43 -0800

    Deleting the state from ResourceList

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 
 class ResourceList extends React.Component {
 
-    state = { resources: [] };
-
     async componentDidMount() {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
         this.setState({ resources: response.data });
