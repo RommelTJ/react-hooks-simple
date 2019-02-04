e27446981d4119dc18c27dca10ba9b6bf6ffa7bb
Rommel Rico <me@rommelrico.com>
2019-02-04 07:12:34 -0800

    Rendering a list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -15,9 +15,9 @@ const ResourceList = ({ resource }) => {
     }, [resource]);
 
     return (
-        <div>
-            {resources.length}
-        </div>
+        <ul>
+            {resources.map(record => <li key={record.id}>{record.title}</li>)}
+        </ul>
     );
 };
 
