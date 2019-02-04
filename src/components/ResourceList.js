473d033649fd71736cbf24c1e9fabbb7eefe00f3
Rommel Rico <me@rommelrico.com>
2019-02-04 06:44:56 -0800

    Deleting componentDidUpdate

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -8,13 +8,6 @@ class ResourceList extends React.Component {
         this.setState({ resources: response.data });
     }
 
-    async componentDidUpdate(prevProps) {
-        if (prevProps.resource !== this.props.resource) {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-            this.setState({ resources: response.data });
-        }
-    }
-
     render() {
         return <div>{this.state.resources.length}</div>;
     }
