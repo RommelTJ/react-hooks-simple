43bfa6e311a661eebb0b0ca1d405ae284527aa59
Rommel Rico <me@rommelrico.com>
2019-02-03 19:03:43 -0800

    Calling the API in ComponentDidMount

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 class ResourceList extends React.Component {
 
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+    }
+
     render() {
         return <div>{this.props.resource}</div>;
     }
