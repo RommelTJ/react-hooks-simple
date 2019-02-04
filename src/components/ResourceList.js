88805b4af3917538585af65c56d2021302152e02
Rommel Rico <me@rommelrico.com>
2019-02-04 07:20:13 -0800

    Removing unused code

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,20 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-const useResources = (resource) => {
-    const [resources, setResources] = useState([]);
-
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-        setResources(response.data);
-    };
-
-    useEffect(() => {
-        fetchResource(resource);
-    }, [resource]);
-
-    return resources;
-};
+import React from 'react';
 
 const ResourceList = ({ resource }) => {
 
