2750fe876abf5d8ec1e6cd5428f6f5a350c8e29c
Rommel Rico <me@rommelrico.com>
2019-02-04 06:52:17 -0800

    Passing the resource into the ResourceList function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
 
     const [resources, setResources] = useState([]);
 
