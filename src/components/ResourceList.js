c82b6cfe41526c1ce573a8824f55de77e8edef69
Rommel Rico <me@rommelrico.com>
2019-02-03 19:16:51 -0800

    Handling updates asynchronously

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -10,6 +10,13 @@ class ResourceList extends React.Component {
         this.setState({ resources: response.data });
     }
 
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+            this.setState({ resources: response.data });
+        }
+    }
+
     render() {
         return <div>{this.state.resources.length}</div>;
     }
