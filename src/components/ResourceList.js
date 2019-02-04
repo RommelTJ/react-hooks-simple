01a7f288ae91f7d630fc6881c8db2c2d2398e62e
Rommel Rico <me@rommelrico.com>
2019-02-04 07:16:28 -0800

    Extracting the logic into a separate function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
-
+const useResources = (resource) => {
     const [resources, setResources] = useState([]);
 
     const fetchResource = async (resource) => {
@@ -13,6 +12,11 @@ const ResourceList = ({ resource }) => {
     useEffect(() => {
         fetchResource(resource);
     }, [resource]);
+
+    return resources;
+};
+
+const ResourceList = ({ resource }) => {
 
     return (
         <ul>
