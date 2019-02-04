2089607695424e13088530d145b8a61a5c80a1d1
Rommel Rico <me@rommelrico.com>
2019-02-03 18:56:24 -0800

    Adding the ResourceList to the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
                 <button onClick={() => setResource('posts')} >Posts</button>
                 <button onClick={() => setResource('todos')}>Todos</button>
             </div>
+            <ResourceList/>
         </div>
     );
 
